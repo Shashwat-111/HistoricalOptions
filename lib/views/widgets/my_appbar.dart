e08e29ffffb81d8a25b9ba1f14427842498504ba
Shashwat-111 <shashwatdubey111@gmail.com>
2024-06-05 14:20:48 +0530

    migrated project to android stuido

--- lib/views/widgets/my_appbar.dart
+++ lib/views/widgets/my_appbar.dart
@@ -1,7 +1,6 @@
 import "package:flutter/material.dart";
 import "package:fno_view/controllers/option_controller.dart";
 import "package:get/get.dart";
-import "package:get/get_state_manager/get_state_manager.dart";
 
 class MyAppBar extends StatefulWidget {
   const MyAppBar({super.key});
