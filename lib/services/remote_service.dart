9f296fbd0e333014346d6ae402689b9b98ebfa2a
Shashwat-111 <shashwatdubey111@gmail.com>
2024-08-09 02:10:50 +0530

    added error handeling and improved readablity

--- lib/services/remote_service.dart
+++ lib/services/remote_service.dart
@@ -4,45 +4,47 @@ import 'package:fno_view/models/graph_data_class.dart';
 import 'package:fno_view/models/strikes_list_class.dart';
 import 'package:http/http.dart' as http;
 
-
 class RemoteService {
+  final _baseUrl = 'https://hardy-antonym-424411-h8.el.r.appspot.com/api';
 
+  // Generic method for GET requests
+  Future<http.Response?> _getRequest(String endpoint) async {
+    try {
+      var uri = Uri.parse('$_baseUrl/$endpoint');
+      debugPrint("Request sent to: $_baseUrl/$endpoint");
+      var response = await http.get(uri);
+      if (response.statusCode == 200) {
+        return response;
+      } else {
+        debugPrint("Failed with status code: ${response.statusCode}");
+      }
+    } catch (e) {
+      debugPrint("Error occurred: $e");
+    }
+    return null;
+  }
 
-  Future<OptionsData?> getFullData(String chartDetails ) async {
-    var client = http.Client();
-    var uri = Uri.parse('https://hardy-antonym-424411-h8.el.r.appspot.com/api/price?$chartDetails');
-    debugPrint("Request sent to : https://hardy-antonym-424411-h8.el.r.appspot.com/api/price?$chartDetails");
-    var response = await client.get(uri);
-    //print(response.body);
-    if(response.statusCode == 200)
-    {
-      var json = response.body;
-      return optionsDataFromJson(json);
+  Future<OptionsData?> getFullData(String chartDetails) async {
+    var response = await _getRequest('price?$chartDetails');
+    if (response != null) {
+      return optionsDataFromJson(response.body);
     }
     return null;
   }
 
   Future<List<ExpiryData>> getExpiryData() async {
-    var client = http.Client();
-    var uri = Uri.parse('https://hardy-antonym-424411-h8.el.r.appspot.com/api/expiries');
-    var response2 = await client.get(uri);
-    if(response2.statusCode == 200)
-    {
-      var json2 = response2.body;
-      return expiryDataFromJson(json2);
+    var response = await _getRequest('expiries');
+    if (response != null) {
+      return expiryDataFromJson(response.body);
     }
     return [];
   }
 
-  Future<List<StrikesData>> getStrikePriceList (String expiry, String right) async {
-      var client = http.Client();
-      var uri = Uri.parse('https://hardy-antonym-424411-h8.el.r.appspot.com/api/strikes?expiry=$expiry&right0=$right');
-      var response3 = await client.get(uri);
-      if(response3.statusCode == 200)
-    {
-      var json2 = response3.body;
-      return strikesDataFromJson(json2);
+  Future<List<StrikesData>> getStrikePriceList(String expiry, String right) async {
+    var response = await _getRequest('strikes?expiry=$expiry&right0=$right');
+    if (response != null) {
+      return strikesDataFromJson(response.body);
     }
     return [];
   }
-  }
+}
