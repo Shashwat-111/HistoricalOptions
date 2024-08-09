cfa07ec2de6048ecee25273f19e2d9f5eedc07d2
Shashwat-111 <shashwatdubey111@gmail.com>
2024-08-09 16:48:33 +0530

    removed currently undisplayable indicators

--- lib/controllers/indicator_controller.dart
+++ lib/controllers/indicator_controller.dart
@@ -5,7 +5,7 @@ class IndicatorController extends GetxController {
 
   //generates a list of 12 bool values, indicating
   // which of the 12 available indicators are active
-  var selectedIndicators = List.generate(12, (_) => false).obs;
+  var selectedIndicators = List.generate(supportedIndicatorList.length, (_) => false).obs;
 
   //updates the value to true/false in the above list
   // based on the current selected indicator
@@ -16,15 +16,15 @@ class IndicatorController extends GetxController {
 
 List<String> supportedIndicatorList = [
   "Bollinger Band",
-  "Relative Strength Index (RSI)",
+  // "Relative Strength Index (RSI)",
   "Simple Moving Average (SMA)",
   "Exponential Moving Average (EMA)",
-  "Moving Average Convergence Divergence (MACD)",
-  "Average True Range (ATR)",
-  "Momentum",
-  "Stochastic",
-  "Accumulation Distribution (AD)",
+  // "Moving Average Convergence Divergence (MACD)",
+  // "Average True Range (ATR)",
+  // "Momentum",
+  // "Stochastic",
+  // "Accumulation Distribution (AD)",
   "Triangular Moving Average (TMA)",
-  "Rate of Change (ROC)",
+  // "Rate of Change (ROC)",
   "Weighted Moving Average (WMA)"
 ];
--- lib/utils/helper_functions.dart
+++ lib/utils/helper_functions.dart
@@ -43,40 +43,46 @@ abstract class HelperFunctions {
 
     List<TechnicalIndicator<dynamic, dynamic>> indicators = [];
     String name = "candle";
+
+    //the indicators that are commented, works, but do not get
+    // displayed on the chart as there value is mostly very lower that
+    // the current visible min and current visible max. So till a solution
+    // is found to display both simultaneous, keeping such indicator commented.
+    //change the [selectedIndicators[index] in 0-12 sequence.
     if (selectedIndicators[0]) {
       indicators.add(BollingerBandIndicator(seriesName: name));
     }
+    // if (selectedIndicators[1]) {
+    //   indicators.add(RsiIndicator<dynamic, dynamic>(seriesName: name));
+    // }
     if (selectedIndicators[1]) {
-      indicators.add(RsiIndicator<dynamic, dynamic>(seriesName: name));
-    }
-    if (selectedIndicators[2]) {
       indicators.add(SmaIndicator<dynamic, dynamic>(period: 5, seriesName: name));
     }
-    if (selectedIndicators[3]) {
+    if (selectedIndicators[2]) {
       indicators.add(EmaIndicator<dynamic, dynamic>(period: 14, seriesName: name));
     }
-    if (selectedIndicators[4]) {
-      indicators.add(MacdIndicator<dynamic, dynamic>(seriesName: name));
-    }
-    if (selectedIndicators[5]) {
-      indicators.add(AtrIndicator<dynamic, dynamic>(period: 3, seriesName: name));
-    }
-    if (selectedIndicators[6]) {
-      indicators.add(MomentumIndicator<dynamic, dynamic>(seriesName: name));
-    }
-    if (selectedIndicators[7]) {
-      indicators.add(StochasticIndicator<dynamic, dynamic>(seriesName: name));
-    }
-    if (selectedIndicators[8]) {
-      indicators.add(AccumulationDistributionIndicator<dynamic, dynamic>(seriesName: name));
-    }
-    if (selectedIndicators[9]) {
+    // if (selectedIndicators[4]) {
+    //   indicators.add(MacdIndicator<dynamic, dynamic>(seriesName: name));
+    // }
+    // if (selectedIndicators[5]) {
+    //   indicators.add(AtrIndicator<dynamic, dynamic>(period: 3, seriesName: name));
+    // }
+    // if (selectedIndicators[6]) {
+    //   indicators.add(MomentumIndicator<dynamic, dynamic>(seriesName: name));
+    // }
+    // if (selectedIndicators[7]) {
+    //   indicators.add(StochasticIndicator<dynamic, dynamic>(seriesName: name));
+    // }
+    // if (selectedIndicators[8]) {
+    //   indicators.add(AccumulationDistributionIndicator<dynamic, dynamic>(seriesName: name));
+    // }
+    if (selectedIndicators[3]) {
       indicators.add(TmaIndicator<dynamic, dynamic>(period: 9, seriesName: name));
     }
-    if (selectedIndicators[10]) {
-      indicators.add(RocIndicator<dynamic, dynamic>(seriesName: name));
-    }
-    if (selectedIndicators[11]) {
+    // if (selectedIndicators[10]) {
+    //   indicators.add(RocIndicator<dynamic, dynamic>(seriesName: name));
+    // }
+    if (selectedIndicators[4]) {
       indicators.add(WmaIndicator<dynamic, dynamic>(period: 5, seriesName: name));
     }
     return indicators;
